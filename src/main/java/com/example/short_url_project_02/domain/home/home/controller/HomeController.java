9635ee3d8772c071ca29fba62bd442446bdbc132
lkhyun-0 <a01024705367@gmail.com>
2025-02-24 12:44:46 +0900

    test

--- src/main/java/com/example/short_url_project_02/domain/home/home/controller/HomeController.java
+++ src/main/java/com/example/short_url_project_02/domain/home/home/controller/HomeController.java
@@ -18,7 +18,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String home() {
-        return "12 Main Page!!! " + customSiteName;
+        return "123456 Main Page!!! " + customSiteName;
     }
 
     @GetMapping("/secretKey")
